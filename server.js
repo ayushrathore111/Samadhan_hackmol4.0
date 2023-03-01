bee22939d8abbf39e2a5ad051758369ba77ba55c
Ayush Rathore <94514710+ayushrathore111@users.noreply.github.com>
2023-03-01 21:39:01 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 /** middlewares */
 app.use(cookieParser());
-app.set('content-type', 'application/json');
+// app.set('content-type', 'application/json');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(morgan('tiny'));
@@ -24,6 +24,10 @@ const port = 3001;
 /** HTTP GET Request */
 
 /** api routes */
+app.get('/',(req,res)=>{
+    res.setHeader('Access-Control-Allow-Credentials','true');
+    res.render('index');
+});
 app.use('/', router);
 app.get('/clubs/gdsc', (req, res) => {
     res.render('clubs');
